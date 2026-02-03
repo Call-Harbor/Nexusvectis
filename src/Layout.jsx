68e5db50a0017844e7fff1a972e268e83ddb9beb
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-02-03 11:41:58 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -56,7 +56,7 @@ const systemMenuItems = [
 ];
 
 export default function Layout({ children, currentPageName }) {
-  const hideNav = currentPageName === "MapMonitor";
+  const hideNav = currentPageName === "MapMonitor" || currentPageName === "Landing";
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
