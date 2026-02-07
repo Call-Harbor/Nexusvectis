6332eb39a07ae126fec269808c02b4f6796c6de6
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-02-07 12:58:56 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -23,6 +23,7 @@ import {
 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import {
   DropdownMenu,
   DropdownMenuContent,
