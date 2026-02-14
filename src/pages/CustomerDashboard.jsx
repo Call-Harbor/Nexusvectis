2d40179a9e7929503e5f8dd66853227e73771349
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-02-14 17:47:36 +0000

    File changes

--- src/pages/CustomerDashboard.jsx
+++ src/pages/CustomerDashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { createPageUrl } from "./utils";
+import { createPageUrl } from "../utils";
 import { base44 } from "@/api/base44Client";
 import { useQuery } from "@tanstack/react-query";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
--- src/pages/CustomerPortal.jsx
+++ src/pages/CustomerPortal.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { createPageUrl } from "./utils";
+import { createPageUrl } from "../utils";
 import { base44 } from "@/api/base44Client";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
