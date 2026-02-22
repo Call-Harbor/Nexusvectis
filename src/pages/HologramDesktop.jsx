d443468c296821b96f166e9bef9d2d5cd4bce53f
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-02-22 15:38:22 +0000

    File changes

--- src/pages/HologramDesktop.jsx
+++ src/pages/HologramDesktop.jsx
@@ -335,7 +335,7 @@ export default function HologramDesktop() {
       </HoloWidget>}
 
       {/* Shipment Tracker */}
-      {activeWidgets.includes('shipments') && <HoloWidget title="Shipment Tracker" icon={Package} color="emerald" defaultPos={{ x: 360, y: 60 }} onClose={() => setActiveWidgets(p => p.filter(w => w !== 'shipments'))}>
+      {activeWidgets.includes('shipments') && <HoloWidget title="Shipment Tracker" icon={Package} color="emerald" defaultPos={getWidgetPosition('shipments', screenIndex)} onClose={() => setActiveWidgets(p => p.filter(w => w !== 'shipments'))}>
         <div className="space-y-2 min-w-[240px]">
           {[
             { label: 'In Transit', count: inTransit.length, color: 'text-cyan-400' },
