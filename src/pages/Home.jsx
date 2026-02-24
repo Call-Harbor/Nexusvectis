dee2096f7b420a90bf18c18aa5d89a3fbf8a9f45
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-02-24 23:53:14 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import { createPageUrl } from "../utils";
 import { 
   Truck, Globe, Zap, Shield, TrendingUp, Satellite,
-  BarChart3, MapPin, Radio, ArrowRight, CheckCircle2, Sparkles, Brain, Orbit, Package
+  BarChart3, MapPin, Radio, ArrowRight, CheckCircle2, Sparkles, Brain, Orbit, Package,
+  Network, Cpu, Wifi, GitBranch, Dna, Bug
 } from "lucide-react";
 import { base44 } from "@/api/base44Client";
 import { useEffect, useState } from "react";
