789fde66e781a4e8f5a800e7695f0d86f449c3ab
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-02-27 16:12:00 +0000

    File changes

--- src/pages/IntellectMode.jsx
+++ src/pages/IntellectMode.jsx
@@ -43,6 +43,7 @@ import IntelligentCommandAgent, { CommandInput, CommandExecution } from "@/compo
 import VideoCallHologram from "@/components/intellect/VideoCallHologram";
 import DeepAnalysisEngine from "@/components/intellect/DeepAnalysisEngine";
 import CourseAIEngine from "@/components/intellect/CourseAIEngine";
+import AICoach from "@/components/intellect/AICoach";
 import AIDocumentEditor from "@/components/intellect/AIDocumentEditor";
 import AISpreadsheetEditor from "@/components/intellect/AISpreadsheetEditor";
 import GlobalSearch from "@/components/intellect/GlobalSearch";
