082bd1c958c697f7aded6ac2286960a42ab262ae
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-02-28 02:18:04 +0000

    File changes

--- src/pages/IntellectMode.jsx
+++ src/pages/IntellectMode.jsx
@@ -23,6 +23,8 @@ import { toast } from "sonner";
 import HologramWindow from "@/components/intellect/HologramWindow";
 import WindowContentRenderer from "@/components/intellect/WindowContentRenderer";
 import { getWindowMeta } from "@/components/intellect/WindowRegistry";
+import IntellectHeader from "@/components/intellect/IntellectHeader";
+import IntellectCommandBar from "@/components/intellect/IntellectCommandBar";
 import NexusSatelliteChat from "@/components/crm/NexusSatelliteChat";
 import FleetGlobe3D from "@/components/intellect/FleetGlobe3D";
 import ThinkingTerminalVisual from "@/components/intellect/ThinkingTerminalVisual";
