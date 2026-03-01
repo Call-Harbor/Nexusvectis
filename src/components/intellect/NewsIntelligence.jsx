d93b3e6c0f7bbb07f08d50a69e989ef070911366
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-01 02:34:17 +0000

    File changes

--- src/components/intellect/NewsIntelligence.jsx
+++ src/components/intellect/NewsIntelligence.jsx
@@ -31,7 +31,7 @@ export default function NewsIntelligence({ openWindow }) {
   const openArticleUrl = (url) => {
     if (!url) return;
     if (openWindow) {
-      openWindow('article_iframe', { x: 160, y: 120 }, { url });
+      openWindow('article_iframe', { x: 160, y: 120 }, { url, __needsClose: true });
     } else {
       setIframeError(false);
       setIframeUrl(url);
