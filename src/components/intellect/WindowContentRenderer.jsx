e04963e562c6d84226463b2f9d1fe16b7218085b
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-02 01:34:22 +0000

    File changes

--- src/components/intellect/WindowContentRenderer.jsx
+++ src/components/intellect/WindowContentRenderer.jsx
@@ -17,6 +17,7 @@ import WebBrowser from "@/components/intellect/WebBrowser";
 import ProfileSearch from "@/components/intellect/ProfileSearch";
 import NewsIntelligence from "@/components/intellect/NewsIntelligence";
 import CompanyAnalysisHologram from "@/components/intellect/CompanyAnalysisHologram";
+import PeopleIntelligenceHologram from "@/components/intellect/PeopleIntelligenceHologram";
 
 const CHART_COLORS = ['#06b6d4', '#8b5cf6', '#10b981', '#f59e0b', '#ef4444'];
 
