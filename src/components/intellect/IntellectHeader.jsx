b90da3ec040adc18d0cdf1d6f7972e03087e51e0
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-04 02:18:34 +0000

    File changes

--- src/components/intellect/IntellectHeader.jsx
+++ src/components/intellect/IntellectHeader.jsx
@@ -253,6 +253,7 @@ export default function IntellectHeader({ orgId, openWindow, executePrompt, setS
               OPERATIONAL
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
