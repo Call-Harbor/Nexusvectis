b1d887e9c0f9b998511665b6c0d9ebf9670d46b8
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-04 02:36:38 +0000

    File changes

--- src/components/intellect/IntellectHeader.jsx
+++ src/components/intellect/IntellectHeader.jsx
@@ -10,7 +10,6 @@ import {
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import CircularBrainMenu from "./CircularBrainMenu";
 import {
   DropdownMenu, DropdownMenuContent, DropdownMenuItem,
   DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger,
