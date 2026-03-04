ba3c8e2cd390131acadc671f836f9dcec1a5eb82
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-04 01:38:42 +0000

    File changes

--- src/components/intellect/WindowContentRenderer.jsx
+++ src/components/intellect/WindowContentRenderer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createPageUrl } from "../../utils";
+import { useQueryClient } from '@tanstack/react-query';
 import { 
   Sparkles, AlertTriangle, TrendingUp, Zap, Activity, Brain
 } from "lucide-react";
