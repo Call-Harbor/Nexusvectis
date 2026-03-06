f141c71aa53a7cab72c812381c523ad052509967
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-06 01:07:33 +0000

    File changes

--- src/components/intellect/WindowContentRenderer.jsx
+++ src/components/intellect/WindowContentRenderer.jsx
@@ -408,6 +408,7 @@ export default function WindowContentRenderer({ type, data, vehicles, routes, sh
   if (type === 'hologram_presentation') return <HologramPresentation orgId={orgId} initialFileUrl={data?.initialFileUrl} onSaved={onSaved} />;
   if (type === 'fleet_ai_trainer') return <FleetAITrainer onClose={data?.onClose} />;
   if (type === 'parallel_processor') return <ParallelTaskProcessor onClose={data?.onClose} externalTasks={data?.tasks || []} />;
+  if (type === 'advanced_intelligence') return <DeepAnalysisEngine vehicles={vehicles} routes={routes} shipments={shipments} alerts={alerts} currentUser={currentUser} orgId={orgId} onInsightCommand={setInput} />;
   if (PAGE_MAP[type]) {
     return <iframe src={`${createPageUrl(PAGE_MAP[type])}?hologram=true`} className="w-full h-full border-0" title={PAGE_MAP[type]} />;
   }
