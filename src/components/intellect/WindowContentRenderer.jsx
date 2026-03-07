53982c0e3b50f4bee11c128f30e2d49ea24cdfe3
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-07 04:04:45 +0000

    File changes

--- src/components/intellect/WindowContentRenderer.jsx
+++ src/components/intellect/WindowContentRenderer.jsx
@@ -411,6 +411,7 @@ export default function WindowContentRenderer({ type, data, vehicles, routes, sh
   if (type === 'fleet_ai_trainer') return <FleetAITrainer onClose={data?.onClose} />;
   if (type === 'parallel_processor') return <ParallelTaskProcessor onClose={data?.onClose} externalTasks={data?.tasks || []} />;
   if (type === 'fleet_3d_viewer') return <Fleet3DViewer onClose={data?.onClose} initialVehicleId={data?.vehicleId} vehicles={vehicles} />;
+  if (type === 'vehicle_builder') return <VehicleBuilder onClose={data?.onClose} />;
   if (PAGE_MAP[type]) {
     return <iframe src={`${createPageUrl(PAGE_MAP[type])}?hologram=true`} className="w-full h-full border-0" title={PAGE_MAP[type]} />;
   }
