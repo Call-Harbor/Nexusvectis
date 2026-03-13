ec60d85d82a285ae4a7a2871cab853cf9ebf45c7
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-13 21:39:22 +0000

    File changes

--- src/components/navigation/CircularNav.jsx
+++ src/components/navigation/CircularNav.jsx
@@ -78,7 +78,7 @@ export default function CircularNav({ currentPageName, user }) {
   };
 
   return (
-    <div className="fixed bottom-6 left-6 z-50">
+    <div className="fixed top-6 left-6 z-50">
       {/* Main Menu Button */}
       <motion.button
         onClick={() => {
