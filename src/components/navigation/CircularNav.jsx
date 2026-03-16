ad6223643cb46fbe6fae1939e56268099fb1df9d
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-16 00:39:45 +0000

    File changes

--- src/components/navigation/CircularNav.jsx
+++ src/components/navigation/CircularNav.jsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 import { 
   Truck, Route, Warehouse, Sparkles, 
   Settings, Users, Shield, FileText, Package, 
-  DollarSign, Target, Menu, X, Zap, Home, AlertCircle
+  DollarSign, Target, Menu, X, Zap, Home, AlertCircle, Code2, BarChart2
 } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import NotificationCenter from "../notifications/NotificationCenter";
