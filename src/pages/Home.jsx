25efdb538cadbf6e9faa5303b788f1fd33423df4
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-18 02:06:46 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1439,7 +1439,7 @@ export default function Home() {
                 </Link>
               </motion.div>
               );
-            })
+            })}
           </div>
 
           <motion.div
