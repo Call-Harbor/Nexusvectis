a52aa8b49154cdb254744d7264a84aee42986375
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-18 02:01:48 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import HolographicInterface from './pages/HolographicInterface';
+import SEODashboard from './pages/SEODashboard';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
