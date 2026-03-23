803a98a21177b288b21bc39bf947f5d0820d79e2
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-23 15:54:22 +0000

    File changes

--- src/components/intellect/WindowContentRenderer.jsx
+++ src/components/intellect/WindowContentRenderer.jsx
@@ -416,6 +416,7 @@ export default function WindowContentRenderer({ type, data, vehicles, routes, sh
   if (type === 'vehicle_builder') return <VehicleBuilder onClose={data?.onClose} />;
   if (type === 'harbor_app_builder') return <HarborAppBuilderV3 onClose={data?.onClose} vehicles={vehicles} routes={routes} shipments={shipments} alerts={alerts} customers={customers} currentUser={currentUser} orgId={orgId} installedAppIds={data?.installedAppIds || new Set()} onInstall={data?.onInstall} />;
   if (type === 'fleet_store') return <FleetStore orgId={orgId} installedIds={Array.from(data?.installedAppIds || [])} onInstall={data?.onInstall} onClose={data?.onClose} />;
+  if (type === 'transit_console') return <iframe src={`${createPageUrl('TransitControl')}?hologram=true`} className="w-full h-full border-0" title="Transit Console" />;
   if (PAGE_MAP[type]) {
     return <iframe src={`${createPageUrl(PAGE_MAP[type])}?hologram=true`} className="w-full h-full border-0" title={PAGE_MAP[type]} />;
   }
