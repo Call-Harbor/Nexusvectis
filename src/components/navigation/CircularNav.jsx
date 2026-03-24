4a89dbce91655cdffcd15a37ab3fd2071cd6ef24
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-24 01:20:59 +0000

    Manual code change

--- src/components/navigation/CircularNav.jsx
+++ src/components/navigation/CircularNav.jsx
@@ -216,7 +216,7 @@ export default function CircularNav({ currentPageName, user }) {
       </AnimatePresence>
 
       {/* Toggle Button */}
-      <div className="fixed bottom-6 left-6 z-50 flex flex-col gap-2">
+      <div className="fixed bottom-7 left-6 z-50 flex flex-col gap-2">
         <motion.button
           onClick={() => { setIsOpen(!isOpen); if (isOpen) setActiveCategory(null); }}
           whileHover={{ scale: 1.05 }}
