81e846e9e0538e3c2abef7937dd89d7e745fb403
cornelio.llambias <cornelio.llambias@nexusvectis.com>
2026-03-26 11:53:15 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -20,7 +20,7 @@ export default function Layout({ children, currentPageName }) {
   }, []);
 
   return (
-    <div className="min-h-screen bg-slate-950">
+    <div className="min-h-screen" style={{ background: 'hsl(var(--background))' }}>
       {!hideNav && (
         <CircularNav currentPageName={currentPageName} user={user} />
       )}
